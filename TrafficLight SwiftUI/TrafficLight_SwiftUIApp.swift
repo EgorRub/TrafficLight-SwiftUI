a41a68232ec754fd51a16c19ed3b573cba33c517
Egor Rubtsov <rubtsove777@gmail.com>
2021-10-27 23:06:12 +0300

    Initial Commit

---
+++ TrafficLight SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight SwiftUI
+//
+//  Created by Егор on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
